5b1b7fe9d287fc88aede9d7bc380a27230e8a416
Tyler Johnston <johnstontyler@protonmail.com>
2025-12-19 21:38:44 +0000

    first commit

---
+++ Interfaces/MainMenu/MainMenu.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public partial class MainMenu : Control
+{
+	public override void _Ready()
+	{
+		var startButton = GetNode<Button>("ButtonContainer/StartButton");
+		var quitButton = GetNode<Button>("ButtonContainer/QuitButton");
+
+		startButton.Pressed += OnStartPressed;
+		quitButton.Pressed += OnQuitPressed;
+	}
+
+	private void OnStartPressed()
+	{
+		GetTree().ChangeSceneToFile("res://PokerGame.tscn");
+	}
+
+	private void OnQuitPressed()
+	{
+		GetTree().Quit();
+	}
+}
---
+++ PokerGame.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+
+public partial class PokerGame : Node2D
+{
+	public override void _Ready()
+	{
+		GD.Print("Poker game started!");
+	}
+
+	public override void _Process(double delta)
+	{
+	}
+}
